2369fd1c0af2985fbbbc0620398b8a40efbe574f
Sanchos <roberto19910619@gmail.com>
2024-11-14 01:22:07 +0100

    .

--- 1_feladat/Concert_Tickets.py
+++ 1_feladat/Concert_Tickets.py
@@ -1,24 +1,22 @@
 mo = list(map(int, input().split(" ")))
-tomb1 = list(map(int, input().split(" ")))
+tomb1 = sorted(list(map(int, input().split())), reverse=True)
 tomb2 = list(map(int, input().split(" ")))
 
-tomb1.sort(reverse=True)
 
 megoldas = []
 i = 0
 j = 0
 
-while i < mo[1] and j < mo[0] and tomb1[j] != 0:
-    if tomb2[i] - tomb1[j] >= 0 and tomb1[j] != 0:
+while i < mo[1] and j < len(tomb1):
+    if tomb2[i] - tomb1[j] >= 0:
         megoldas.append(tomb1[j])
-        tomb1[j] = 0
+        del tomb1[j]
         i += 1
         j = 0
-        tomb1.sort(reverse=True)
         continue
     j += 1
 
-megoldas.extend(((len(tomb2)-tomb1.count(0)) * [-1]))
+megoldas.extend(((len(tomb2) - i) * [-1]))
 
 for i in range(len(megoldas)):
     print(megoldas[i])
